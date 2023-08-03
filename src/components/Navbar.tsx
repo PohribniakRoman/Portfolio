a6775c799e8112b0bda740bd4c8ce08fb7a8f530
PohribniakRoman <79878288+PohribniakRoman@users.noreply.github.com>
2023-08-03 13:17:51 +0300

    laptop

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,19 @@
+import { useState } from "react"
+
 const scrollBtn = (block:string) => {
     document.querySelector(`.${block}`)?.scrollIntoView({block:"center",behavior:"smooth"})
 }
 export const Navbar:React.FC = () => {
+    const [isActive,setActive] = useState<boolean>(false)
+
     return <nav className="navbar">
         <nav className="navbar--logo">Roman.dev</nav>
-        <nav className="navbar--menu">
-            <nav className="navbar--menu-item" onClick={()=>scrollBtn("banner")}>Home</nav>
-            <nav className="navbar--menu-item" onClick={()=>scrollBtn("about")}>About</nav>
-            <nav className="navbar--menu-item" onClick={()=>scrollBtn("projects")}>Projects</nav>
-            <nav className="navbar--menu-item" onClick={()=>scrollBtn("contact")}>Contact</nav>
+        <nav className={`navbar--menu-icon ${isActive?"active":""}`} onClick={()=>{setActive(!isActive)}}><i/></nav>
+        <nav className={`navbar--menu ${isActive?"active":""}`}>
+            <nav className="navbar--menu-item" onClick={()=>{scrollBtn("banner");setActive(!isActive)}}>Home</nav>
+            <nav className="navbar--menu-item" onClick={()=>{scrollBtn("about");setActive(!isActive)}}>About</nav>
+            <nav className="navbar--menu-item" onClick={()=>{scrollBtn("projects");setActive(!isActive)}}>Projects</nav>
+            <nav className="navbar--menu-item" onClick={()=>{scrollBtn("contact");setActive(!isActive)}}>Contact</nav>
         </nav>
     </nav>
 }
