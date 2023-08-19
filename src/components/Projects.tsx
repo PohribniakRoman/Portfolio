c4ed9e038c8d1c63609641b6662dc4406a3506ca
PohribniakRoman <79878288+PohribniakRoman@users.noreply.github.com>
2023-08-19 15:17:32 +0300

    color picker

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,5 +1,6 @@
 import mealCover from "../assets/meal-app.jpg";
 import galleryCover from "../assets/gallery.jpg";
+import colorPickerCover from "../assets/colorpicker.jpg";
 
 import { ProjectItem } from "./ProjectItem"
 
@@ -9,7 +10,7 @@ const projects = [
     description:"The web application is built without using third-party APIs. When a page is loaded, all media elements are loaded into the browser cache. All animations were written manually.",
     demo:"https://gallery-pohribniakroman.vercel.app/",
     git:"https://github.com/PohribniakRoman/Gallery",
-    stack:["React","Redux","SCSS"],
+    stack:["React","Redux","SCSS","TypeScript"],
     cover:galleryCover
 },
 {
@@ -17,8 +18,16 @@ const projects = [
     description:"A web application with many interactions, such as a draggable border between the cart and store, ingredient hover effect, search and billing system.",
     demo:"https://delivery-app-pohribniakroman.vercel.app/",
     git:"https://github.com/PohribniakRoman/Meal-App",
-    stack:["React","Redux","SCSS","API's"],
+    stack:["React","Redux","SCSS","API's","TypeScript"],
     cover:mealCover
+},
+{
+    name:"Color Picker ✏️",
+    description:"A service where you can upload your image via a link/file or just select a color on the screen to choose a color from it. You can also convert this color to any format and see shades & tints.",
+    demo:"https://color-picker-tan.vercel.app/",
+    git:"https://github.com/PohribniakRoman/Color-Picker",
+    stack:["React","Redux","SCSS","TypeScript"],
+    cover:colorPickerCover
 },
 ]
 
